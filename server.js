9fec7740e0ec17adf112aad768485dbec6cae79d
omarsuarezrodriguez <omar.suarez.rod@gmail.com>
2023-03-30 23:13:21 -0500

    0.23.0

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
 
 
 const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
+
+app.disable('x-powered-by');
+
 app.set('port', port);
 
 server.listen(3000, '192.168.56.1' || 'localhost', function() {
@@ -12,3 +24,24 @@ server.listen(3000, '192.168.56.1' || 'localhost', function() {
     console.log('Aplicación de NodeJS  - Proceso: ' + process.pid);
     console.log('Aplicación de NodeJS - Puerto: ' + port);
 });
+
+
+app.get('/', (req, res) => {
+    res.send('Ruta raiz del backend');
+});
+
+app.get('/test', (req, res) => {
+    res.send('Esta es la ruta Test');
+});
+
+
+// Error Handler
+app.use((error, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
+});
+
+
+// 200 - Es una respuesta exitosa
+// 404 - Significa que la URL no existe
+// 500 - Error interno del servidor (código)
