148891398d9abe242328358a3afbc10746f06dfb
omarsuarezrodriguez <omar.suarez.rod@gmail.com>
2023-03-30 22:46:35 -0500

    Se agrega el proyecto BackendDeliveryAppUdemy

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+server.listen(3000, '192.168.56.1' || 'localhost', function() {
+    console.log('Aplicación de NodeJS  - Proceso: ' + process.pid + ' Iniciada...');
+    console.log('Aplicación de NodeJS - Puerto: ' + port + ' Iniciada...');
+});
